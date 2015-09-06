4d084a41d9cdf8677c150edb7d069865cc290ee0
Toan Nguyen <nntoan@protonmail.com>
2015-09-06 17:27:18 +0700

    Update tools/upgrade.sh

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -9,7 +9,7 @@ then
   printf '\033[0;32m%s\033[0m\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
   printf '\033[0;32m%s\033[0m\n' '                        /____/                       '
   printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh Custom has been updated and/or is at the current version.'
-  printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest news and updates, follow us on github: ' 'https://github.io/nntoan/oh-my-zsh-custom'
+  printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest news and updates, follow us on github: ' 'https://github.com/nntoan/oh-my-zsh-custom'
 else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 fi
