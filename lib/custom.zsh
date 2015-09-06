a7c83d3491b62fea790392297a352395481c00b3
Toan Nguyen <nntoan@protonmail.com>
2015-09-06 17:25:47 +0700

    Fix minor bug with source files in lib/

--- lib/custom.zsh
+++ lib/custom.zsh
@@ -1,6 +1,6 @@
 export OSX_SUPERB="$ZSH_CUSTOM/plugins/osx-superb"
 export SUPERB_ALIAS="$ZSH_CUSTOM/plugins/superb-aliases"
 
-function upgrade_omz_custom() {
+function upgrade_oh_my_zsh_custom() {
   env ZSH_CUSTOM=$ZSH_CUSTOM /bin/sh $ZSH_CUSTOM/tools/upgrade.sh
 }
--- oh-my-zsh-custom.zsh
+++ oh-my-zsh-custom.zsh
@@ -14,3 +14,15 @@ if [ "$ITERM2_SHELL_INTEGRATION" != "true" ]; then
     env ZSH_CUSTOM=$ZSH_CUSTOM ITERM2_SHELL_INTEGRATION=$ITERM2_SHELL_INTEGRATION
     source $ZSH_CUSTOM/tools/iterm2_shell_integration.zsh
 fi
+
+# Set ZSH_CACHE_DIR to the path where cache files should be created
+# or else we will use the default cache/
+if [[ -z "$ZSH_CUSTOM_CACHE_DIR" ]]; then
+  ZSH_CUSTOM_CACHE_DIR="$ZSH_CUSTOM/cache/"
+fi
+
+
+# Load all of the config files in ~/.oh-my-zsh-custom/lib that end in .zsh
+for config_file ($ZSH_CUSTOM/lib/*.zsh); do
+  source $config_file
+done
