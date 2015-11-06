c956057cab7a006489b01663eff5cae86f1ca06f
Toan Nguyen <nntoan@protonmail.com>
2015-11-06 17:25:37 +0700

    Add new plugin laravel-cmd

---
+++ plugins/laravel-cmd/laravel-cmd.plugin.zsh
@@ -0,0 +1,7 @@
+# Generator Stuff
+alias g:m="la4 generate:model"
+alias g:c="la4 generate:controller"
+alias g:v="la4 generate:view"
+alias g:s="la4 generate:seed"
+alias g:mig="la4 generate:migration"
+alias g:r="la4 generate:resource"
