236b318891c7a0683bcfa221899f58fbcb869fb2
Toan Nguyen <nntoan@protonmail.com>
2017-07-16 09:58:47 +0700

    Update nanorc files and upgrade.sh Slant font

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -5,12 +5,12 @@ fi
 cd "$ZSH_CUSTOM"
 if git pull --rebase --stat origin master
 then
-  printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
-  printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
-  printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
-  printf '\033[0;32m%s\033[0m\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
-  printf '\033[0;32m%s\033[0m\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
-  printf '\033[0;32m%s\033[0m\n' '                        /____/                       '
+  printf '\033[0;32m%s\033[0m\n' '         __                                     __                       __                '
+  printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_     _______  _______/ /_____  ____ ___ '
+  printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \   / ___/ / / / ___/ __/ __ \/ __ `__ \'
+  printf '\033[0;32m%s\033[0m\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / /  / /__/ /_/ (__  ) /_/ /_/ / / / / / /'
+  printf '\033[0;32m%s\033[0m\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/   \___/\__,_/____/\__/\____/_/ /_/ /_/ '
+  printf '\033[0;32m%s\033[0m\n' '                        /____/                                                             '
   printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh Custom has been updated and/or is at the current version.'
   printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest news and updates, follow us on github: ' 'https://github.com/nntoan/oh-my-zsh-custom'
 else
